21ef2328ee836fa6e66ab3e06c5229354a772bb8
achiradesilva123 <achiradesilva123@gmail.com>
2021-08-07 12:55:34 +0500

    added relase version

--- db/DbConnection.java
+++ db/DbConnection.java
@@ -2,6 +2,7 @@
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
+ *  *  @version 1.0.0
  */
 package config;
 
@@ -11,25 +12,25 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 /**
- *
- * @author shanilmiranda
+ *shanilmiranda
+ * @author
  */
 public class DbConnection {
     private static DbConnection dBConnection;
     private Connection con;
-    
-    
+
+
     private DbConnection() throws ClassNotFoundException, SQLException{
         Class.forName("com.mysql.cj.jdbc.Driver");
         con=DriverManager.getConnection("jdbc:mysql://localhost:3306/pos_ijse","root","root");
     }
-    
+
     public static DbConnection getInstance() throws ClassNotFoundException, SQLException{
         return dBConnection==null ? dBConnection=new DbConnection() : dBConnection;
     }
-    
+
     public Connection getConnection(){
         return con;
     }
-    
+
 }
