13774f1a5a4b5868085819b91b3b50b6a8553b46
achiradesilva123 <achiradesilva123@gmail.com>
2021-08-07 12:04:14 +0500

    added singleton db

--- db/DbConnection.java
+++ db/DbConnection.java
@@ -12,24 +12,24 @@ import java.sql.SQLException;
 
 /**
  *
- * @author shanilmiranda
+ * @author achira De Silva
  */
 public class DbConnection {
     private static DbConnection dBConnection;
     private Connection con;
-    
-    
+
+
     private DbConnection() throws ClassNotFoundException, SQLException{
         Class.forName("com.mysql.cj.jdbc.Driver");
         con=DriverManager.getConnection("jdbc:mysql://localhost:3306/pos_ijse","root","root");
     }
-    
+
     public static DbConnection getInstance() throws ClassNotFoundException, SQLException{
         return dBConnection==null ? dBConnection=new DbConnection() : dBConnection;
     }
-    
+
     public Connection getConnection(){
         return con;
     }
-    
+
 }
